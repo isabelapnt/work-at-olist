3f0479a81c36554511cd799b7c5ea8cc4c4465df
Isabela Pinto <isabela.souza@cargobr.com>
2018-09-04 01:20:37 -0300

    Telephone Bill models

--- challenge/apps/call_record/models.py
+++ challenge/apps/call_record/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from model_utils import Choices
+from decimal import Decimal
 
 
 class CallRecord(models.Model):
@@ -31,3 +32,13 @@ class EndRecord(CallRecord):
         verbose_name_plural = 'Call End Records'
 
     start_record = models.OneToOneField('StartRecord', on_delete=models.CASCADE)
+
+
+class TelephoneBill(models.Model):
+    class Meta:
+        verbose_name = 'Telephone Bill'
+        verbose_name_plural = 'Telephone Bills'
+
+    start_record = models.ForeignKey('StartRecord', null=True, on_delete=models.SET_NULL)
+    duration = models.TimeField('Call Duration', null=True)
+    price = models.DecimalField('Call Price', max_digits=8, decimal_places=2, default=Decimal())
